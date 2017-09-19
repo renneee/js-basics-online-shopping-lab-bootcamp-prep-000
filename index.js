72cde22a68acce43d4831765275850dfb939ab30
renneee <renpyang@gmail.com>
2017-09-19 05:12:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,15 +61,20 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(item)){
-      cart.splice(i,1);
+  let itemInCart = false;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
+    }
   }
 
-    if (!cart[i].hasOwnProperty(item)) {
-    console.log (`That item is not in your cart.`);
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
   }
-}
+
   return cart;
 }
 
