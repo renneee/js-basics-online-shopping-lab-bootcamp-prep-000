fbf8252abe8be302014f4ddc9b5f67adfde90a19
renneee <renpyang@gmail.com>
2017-09-19 04:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,13 +75,11 @@ function removeFromCart(item) {
 }
 
 
-
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    console.log("Sorry, we don't have a credit card on file for you.");
-    return false;
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+  } else {
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
   }
-
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-
   cart = [];
+}
